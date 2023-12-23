1897290a791b0f1af87a47049dfee530539231bf
OndrejMottl <ondrej.mottl@gmail.com>
2023-12-23 21:54:24 +0100

    Update Landgenerator and Tile classes: rework the spawning system

--- Assets/Landgenerator.cs
+++ Assets/Landgenerator.cs
@@ -8,18 +8,21 @@ public class Landgenerator : MonoBehaviour
 
     public float wordSizeX = 10;
     public float wordSizeZ = 10;
-    public float wordSizeY = 10;
-
+    public float wordSizeY = 3;
+    public Tile[,] tiles;
     private float tileSize;
 
     // Start is called before the first frame update
     void Start()
     {
+        // create a new array of tiles
+        tiles = new Tile[(int)wordSizeX, (int)wordSizeZ];
+
         // get the size of the tile
         tileSize = defaultTile.GetComponent<Renderer>().bounds.size.x;
 
         // generate the world
-        GenerateWorld(wordSizeX, wordSizeZ, tileSize);
+        GenerateWorld(wordSizeX, wordSizeZ, wordSizeY, tileSize);
     }
 
     // Update is called once per frame
@@ -28,29 +31,30 @@ public class Landgenerator : MonoBehaviour
 
     }
 
-    void SpawnTile(float x, float z)
+    void SpawnTile(float x, float z, float y)
     {
         // create a new tile
         Tile newTile = Instantiate(defaultTile) as Tile;
 
         // set the position of the tile
-        newTile.transform.position = new Vector3(x, 0, z);
+        newTile.transform.position = new Vector3(x, y, z);
 
         // set the parent of the tile
         newTile.transform.parent = transform;
     }
 
     // generate the world with hexes
-    void GenerateWorld(float xSize, float zSize, float tileSize)
+    void GenerateWorld(float xSize, float zSize, float ySize, float tileSize)
     {
         // get half the size of the tile
         float tileSizeHalf = tileSize / 2;
 
         // get the offset of the tile
-        float tileOffset = tileSize / 10;
+        float tileOffset = tileSize / 25;
 
         float xPosition = 0;
         float zPosition = 0;
+        float yPosition = 0;
 
         // print in console the size of the tile, halfise and tile offset   
         Debug.Log("tileSize: " + tileSize + " tileSizeHalf: " + tileSizeHalf + " tileOffset: " + tileOffset);
@@ -68,20 +72,30 @@ public class Landgenerator : MonoBehaviour
                 if (z % 2 == 0)
                 {
                     xPosition = (tileSize * x) + (tileOffset * x);
-                    zPosition = (tileSize * z);
+
                 }
 
                 else if (z % 2 != 0)
                 {
-                    xPosition = (tileSize * x) - tileSizeHalf + (tileOffset * x);
-                    zPosition = (tileSize * z);
+                    xPosition = (tileSize * x) + tileSizeHalf + (tileOffset * x);
                 }
 
-                // print in console the x and z position
-                Debug.Log("x position: " + xPosition + " z position: " + zPosition);
+                zPosition = ((float)(tileSize * z * 0.9));
 
-                // spawn a tile at the position
-                SpawnTile(xPosition, zPosition);
+                // assing yPosition as random number betwen 1 and ySize
+                yPosition = Random.Range(0, ySize) * tileOffset;
+
+                // print in console the x and z position
+                Debug.Log(
+                    "x position: " + xPosition +
+                    " z position: " + zPosition +
+                    " y position: " + yPosition
+                    );
+
+                // add a Tile to the array
+                Tile newTile = Instantiate(defaultTile) as Tile;
+                newTile.SetPosition(xPosition, zPosition, yPosition);
+                tiles[x, z] = newTile;
             }
         }
     }
--- Assets/Tile.cs
+++ Assets/Tile.cs
@@ -1,13 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class Tile : MonoBehaviour
 {
+    public float xPosition;
+    public float zPosition;
+    public float yPosition;
+
+    public void SetPosition(float xPosition, float zPosition, float yPosition)
+    {
+        this.xPosition = xPosition;
+        this.zPosition = zPosition;
+        this.yPosition = yPosition;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // set the position of the tile
+        transform.position = new Vector3(xPosition, yPosition, zPosition);
     }
 
     // Update is called once per frame
